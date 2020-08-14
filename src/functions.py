9c727732d6862d79504d25457f321e1a061ed8f3
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2020-08-14 13:30:36 +1000

    Customise functions file

--- src/functions.py
+++ src/functions.py
@@ -2,15 +2,17 @@ import numpy as np
 import scipy.sparse as sp
 import torch
 
-def load_data_monti(M, testing=False, rating_map=None, post_rating_map=None):
-    num_col = M.shape[0]
-    num_row = M.shape[1]
+def matrix_labels(M, testing=False, rating_map=None, post_rating_map=None):
+    num_users = M.shape[0]
+    num_items = M.shape[1]
 
     u_nodes_ratings = np.where(M)[0]
     v_nodes_ratings = np.where(M)[1]
+
     ratings = M[np.where(M)]
 
-    u_nodes_ratings, v_nodes_ratings = u_nodes_ratings.astype(np.int64), v_nodes_ratings.astype(np.int32)
+    u_nodes_ratings, v_nodes_ratings = u_nodes_ratings.astype(
+        np.int64), v_nodes_ratings.astype(np.int32)
     ratings = ratings.astype(np.float64)
 
     u_nodes = u_nodes_ratings
@@ -25,31 +27,27 @@ def load_data_monti(M, testing=False, rating_map=None, post_rating_map=None):
     rating_dict = {r: i for i, r in enumerate(
         np.sort(np.unique(ratings)).tolist())}
 
-    labels = np.full((num_col, num_row), neutral_rating, dtype=np.int32)
+    labels = np.full((num_users, num_items), neutral_rating, dtype=np.int32)
     labels[u_nodes, v_nodes] = np.array([rating_dict[r] for r in ratings])
+
 
     for i in range(len(u_nodes)):
         assert(labels[u_nodes[i], v_nodes[i]] == rating_dict[ratings[i]])
 
     labels = labels.reshape([-1])
 
-    # number of test and validation edges
-
+    # number training
     num_train = np.where(Otraining)[0].shape[0]
-    num_test = np.where(Otest)[0].shape[0]
     num_val = int(np.ceil(num_train * 0.2))
     num_train = num_train - num_val
 
+    # non_zero arrays
     pairs_nonzero_train = np.array([[u, v] for u, v in zip(
         np.where(Otraining)[0], np.where(Otraining)[1])])
-    idx_nonzero_train = np.array([u * num_row + v for u, v in pairs_nonzero_train])
+    idx_nonzero_train = np.array(
+        [u * num_items + v for u, v in pairs_nonzero_train])
 
-    pairs_nonzero_test = np.array(
-        [[u, v] for u, v in zip(np.where(Otest)[0], np.where(Otest)[1])])
-    idx_nonzero_test = np.array(
-        [u * num_row + v for u, v in pairs_nonzero_test])
-
-    # Internally shuffle training set (before splitting off validation set)
+     # Internally shuffle training set (before splitting off validation set)
     rand_idx = list(range(len(idx_nonzero_train)))
     np.random.seed(42)
     np.random.shuffle(rand_idx)
@@ -57,47 +55,30 @@ def load_data_monti(M, testing=False, rating_map=None, post_rating_map=None):
     pairs_nonzero_train = pairs_nonzero_train[rand_idx]
 
     idx_nonzero = np.concatenate([idx_nonzero_train, idx_nonzero_test], axis=0)
-    pairs_nonzero = np.concatenate([pairs_nonzero_train, pairs_nonzero_test], axis=0)
+    pairs_nonzero = np.concatenate(
+        [pairs_nonzero_train, pairs_nonzero_test], axis=0)
 
-    val_idx = idx_nonzero[0:num_val]
     train_idx = idx_nonzero[num_val:num_train + num_val]
-    test_idx = idx_nonzero[num_train + num_val:]
-
-    assert(len(test_idx) == num_test)
-
-    val_pairs_idx = pairs_nonzero[0:num_val]
     train_pairs_idx = pairs_nonzero[num_val:num_train + num_val]
-    test_pairs_idx = pairs_nonzero[num_train + num_val:]
-
-    u_test_idx, v_test_idx = test_pairs_idx.transpose()
-    u_val_idx, v_val_idx = val_pairs_idx.transpose()
     u_train_idx, v_train_idx = train_pairs_idx.transpose()
 
-    # create labels
     train_labels = labels[train_idx]
-    val_labels = labels[val_idx]
-    test_labels = labels[test_idx]
-
-    if testing:
-        u_train_idx = np.hstack([u_train_idx, u_val_idx])
-        v_train_idx = np.hstack([v_train_idx, v_val_idx])
-        train_labels = np.hstack([train_labels, val_labels])
-        # for adjacency matrix construction
-        train_idx = np.hstack([train_idx, val_idx])
 
     class_values = np.sort(np.unique(ratings))
 
-    # make training adjacency matrix
-    rating_mx_train = np.zeros(num_col * num_row, dtype=np.float32)
     '''Note here rating matrix elements' values + 1 !!!'''
+
+    rating_mx_train = np.zeros(num_users * num_items, dtype=np.float32)
+
     if post_rating_map is None:
         rating_mx_train[train_idx] = labels[train_idx].astype(np.float32) + 1.
     else:
         rating_mx_train[train_idx] = np.array(
             [post_rating_map[r] for r in class_values[labels[train_idx]]]) + 1.
 
+
     rating_mx_train = sp.csr_matrix(
-        rating_mx_train.reshape(num_col, num_row))
+        rating_mx_train.reshape(num_users, num_items))
 
     if u_features is not None:
         u_features = sp.csr_matrix(u_features)
@@ -107,4 +88,4 @@ def load_data_monti(M, testing=False, rating_map=None, post_rating_map=None):
         v_features = sp.csr_matrix(v_features)
         print("Item features shape: " + str(v_features.shape))
 
-    return u_features, v_features, rating_mx_train, train_labels, u_train_idx, v_train_idx, val_labels, u_val_idx, v_val_idx, test_labels, u_test_idx, v_test_idx, class_values
+    return u_features, v_features, rating_mx_train, train_labels, u_train_idx, v_train_idx, class_values
